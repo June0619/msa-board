d27244d0dbcf5ba25df61f8bcd1420f65260a1fb
June0619 <jjw5050@hotmail.com>
2025-05-08 21:01:11 +0900

    initial commit

---
+++ src/test/java/hello/board/MsaBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsaBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
