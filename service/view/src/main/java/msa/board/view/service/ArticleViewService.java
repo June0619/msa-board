87832da6588f25bb1aa8a69d6b2e49e211921f22
June0619 <jjw5050@hotmail.com>
2025-10-19 18:33:33 +0900

    조회수 어뷰징 방지 구현

--- service/view/src/main/java/msa/board/view/service/ArticleViewService.java
+++ service/view/src/main/java/msa/board/view/service/ArticleViewService.java
@@ -1,18 +1,28 @@
 package msa.board.view.service;
 
+import java.time.Duration;
+
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
 import msa.board.view.repository.ArticleViewCountRepository;
+import msa.board.view.repository.ArticleViewDistributedLockRepository;
 
 @Service
 @RequiredArgsConstructor
 public class ArticleViewService {
 	private final ArticleViewCountRepository articleViewCountRepository;
 	private final ArticleViewCountBackUpProcessor backUpProcessor;
+	private final ArticleViewDistributedLockRepository distributedLockRepository;
+
 	private static final int BACK_UP_BATCH_SIZE = 100;
+	private static final Duration TTL = Duration.ofMinutes(10);
 
 	public Long increase(Long articleId, Long userId) {
+		if (!distributedLockRepository.lock(articleId, userId, TTL)) {
+			return articleViewCountRepository.read(articleId);
+		}
+
 		Long count = articleViewCountRepository.increase(articleId);
 		if (count % BACK_UP_BATCH_SIZE == 0) {
 			backUpProcessor.backUp(articleId, count);
