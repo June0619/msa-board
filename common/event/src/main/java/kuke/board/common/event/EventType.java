176d04e4d9e0996d841ab0c70adc6b1d0c08ab43
June0619 <jjw5050@hotmail.com>
2025-10-21 12:58:33 +0900

    Event&Serializer

---
+++ common/event/src/main/java/kuke/board/common/event/EventType.java
@@ -0,0 +1,48 @@
+package kuke.board.common.event;
+
+import kuke.board.common.event.payload.ArticleCreatedEventPayload;
+import kuke.board.common.event.payload.ArticleDeletedEventPayload;
+import kuke.board.common.event.payload.ArticleLikedEventPayload;
+import kuke.board.common.event.payload.ArticleUnlikedEventPayload;
+import kuke.board.common.event.payload.ArticleUpdatedEventPayload;
+import kuke.board.common.event.payload.ArticleViewedEventPayload;
+import kuke.board.common.event.payload.CommentCreatedEventPayload;
+import kuke.board.common.event.payload.CommentDeletedEventPayload;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Getter
+@RequiredArgsConstructor
+public enum EventType {
+
+	ARTICLE_CREATED(ArticleCreatedEventPayload.class, Topic.JWJUNG_BOARD_ARTICLE),
+	ARTICLE_UPDATED(ArticleUpdatedEventPayload.class, Topic.JWJUNG_BOARD_ARTICLE),
+	ARTICLE_DELETED(ArticleDeletedEventPayload.class, Topic.JWJUNG_BOARD_ARTICLE),
+	COMMENT_CREATED(CommentCreatedEventPayload.class, Topic.JWJUNG_BOARD_COMMENT),
+	COMMENT_DELETED(CommentDeletedEventPayload.class, Topic.JWJUNG_BOARD_COMMENT),
+	ARTICLE_LIKED(ArticleLikedEventPayload.class, Topic.JWJUNG_BOARD_LIKE),
+	ARTICLE_UNLIKED(ArticleUnlikedEventPayload.class, Topic.JWJUNG_BOARD_LIKE),
+	ARTICLE_VIEWED(ArticleViewedEventPayload.class, Topic.JWJUNG_BOARD_VIEW);
+
+	private final Class<? extends EventPayload> payloadClass;
+	private final String topic;
+
+	public static EventType from(String type) {
+		try {
+			return valueOf(type);
+		} catch (Exception e) {
+			log.error("[EventType.from] type={}", type, e);
+			return null;
+		}
+	}
+
+	public static class Topic {
+		public static final String JWJUNG_BOARD_ARTICLE = "jwjung-board_article";
+		public static final String JWJUNG_BOARD_COMMENT = "jwjung-board-comment";
+		public static final String JWJUNG_BOARD_LIKE = "jwjung-board-like";
+		public static final String JWJUNG_BOARD_VIEW = "jwjung-board-view";
+	}
+
+}
