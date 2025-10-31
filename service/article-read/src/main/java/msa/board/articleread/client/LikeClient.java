58ecf1b488b323dda330e8d81484200e2fcc484d
June0619 <jjw5050@hotmail.com>
2025-10-31 13:03:21 +0900

    client & query model

---
+++ service/article-read/src/main/java/msa/board/articleread/client/LikeClient.java
@@ -0,0 +1,37 @@
+package msa.board.articleread.client;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClient;
+
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class LikeClient {
+
+	private RestClient restClient;
+
+	@Value("${endpoints.jwjung-board-comment-service.url}")
+	private String commentServiceUrl;
+
+	@PostConstruct
+	public void initRestClient() {
+		restClient = RestClient.create(commentServiceUrl);
+	}
+
+	public long read(Long articleId) {
+		try {
+			return restClient.get()
+					.uri("/v1/article-likes/articles/{articleId}/count")
+					.retrieve()
+					.body(Long.class);
+		} catch (Exception e) {
+			log.error("[LikeClient.read] articleId={}", articleId, e);
+			return 0;
+		}
+	}
+}
---
+++ service/article-read/src/main/java/msa/board/articleread/client/ViewClient.java
@@ -0,0 +1,37 @@
+package msa.board.articleread.client;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClient;
+
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class ViewClient {
+
+	private RestClient restClient;
+
+	@Value("${endpoints.jwjung-board-view-service.url}")
+	private String viewServiceUrl;
+
+	@PostConstruct
+	public void initRestClient() {
+		restClient = RestClient.create(viewServiceUrl);
+	}
+
+	public long read(Long articleId) {
+		try {
+			return restClient.get()
+					.uri("/v1/article-views/aritlces/{articleId}/count", articleId)
+					.retrieve()
+					.body(Long.class);
+		} catch (Exception e) {
+			log.error("[ViewClient.read] articleId={}", articleId, e);
+			return 0;
+		}
+	}
+}
